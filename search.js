11accbd560601753f8f0227697adbde3e4e14359
Kenny Yu <yu.kenny124@gmail.com>
2013-10-20 02:07:27 -0400

    tab redirect works

--- search.js
+++ search.js
@@ -9,14 +9,40 @@ MAX_SEARCH_RESULTS = 10;
 chrome.omnibox.onInputChanged.addListener(
   function(text, suggest) {
     console.log('inputChanged: ' + text);
-    suggest([
-      {content: text + " one", description: "the first one"},
-      {content: text + " number two", description: "the second entry"}
-    ]);
+//    suggest([
+//      {content: text + " one", description: "the first one"},
+//      {content: text + " number two", description: "the second entry"}
+//    ]);
+    gapi.client.load('drive', 'v2', function() {
+      request = gapi.client.request({
+        'path': '/drive/v2/files',
+        'method': 'GET',
+        'params': {
+  //        'q' : 'CS261',
+          'maxResults': MAX_SEARCH_RESULTS,
+        }
+      });
+      request.execute(function(response) {
+        console.log(response);
+        suggestions = [];
+        for (var i in response.items) {
+          item = response.items[i];
+          suggestions.push({
+            content: item.title,
+            description: "<url>%s</url>".format(item.alternateLink),
+          });
+        }
+        suggest(suggestions);
+      });
+    });
   });
 
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
     console.log('inputEntered: ' + text);
-    alert('You just typed "' + text + '"');
+    chrome.tabs.query(
+      {active: true, currentWindow: true},
+      function(tabs) {
+        chrome.tabs.update(tabs[0].id, {url: text});
+      });
   });
